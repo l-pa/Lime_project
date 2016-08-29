6a8e576fbbe29eb5ceb67ae2c80d17a191f6597a
Lukáš Papík <papikdev@gmail.com>
2016-08-29 19:38:13 +0200

    Draw update
    Draw with rectangles, nothing special

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import pygame
 from pygame import gfxdraw
 from pygame.locals import *
-import sys
 
 
 def main():
@@ -12,7 +11,18 @@ def main():
     test_pos = 50, 50
     mouse_x = 0
     mouse_y = 0
-    max_FPS = 60
+    max_FPS = 144
+    brush_color = (255, 255, 255)
+    brush_size = 1
+
+    #Colors
+    color_one = (255, 0, 0)
+    color_two = (0, 255, 0)
+    color_three = (0, 0, 255)
+    color_four = (255, 255, 0)
+    color_five = (255, 0, 255)
+    color_six = (0, 255, 255)
+    color_zero = (255, 255, 255)
 
     # Initialise
     pygame.init()
@@ -20,6 +30,7 @@ def main():
     pygame.display.set_caption(windowName)
     clock = pygame.time.Clock()
     clock.tick(max_FPS)
+
 
     # Fill background
     background = pygame.Surface(screen.get_size())
@@ -32,9 +43,13 @@ def main():
         background_color = colorB, colorG, colorR
         return background_color
 
+    def inverted_rgb():
+        color = RGB_mouse_color(800,600)
+        return color
+
     # Blit everything to the screen
-    screen.blit(background, (0, 0))
-    pygame.display.flip()
+    # screen.blit(background, (0, 0))
+    # pygame.display.flip()
 
     #Rectangle
     def draw_box(x, y, width, height, surface, box_color):
@@ -42,21 +57,53 @@ def main():
         pygame.gfxdraw.box(surface, rectangle, box_color)
 
 
+    # Display some text
+    font = pygame.font.Font(None, 30)
+    text = font.render(str(pygame.display.get_driver()), 1, (255, 255, 255))
+    textpos = text.get_rect()
+    textpos.centerx = background.get_rect().centerx
+
     # Event loop
     while 1:
+        pressed = pygame.key.get_pressed()
+
         for event in pygame.event.get():
             if event.type == QUIT:
                 return
+            if pressed[pygame.K_x]:
+                background.fill(RGB_mouse_color(800, 600))
 
-            if event.type == pygame.MOUSEBUTTONDOWN:
-                print('Click')
+            if pygame.mouse.get_pressed() == (1, 0, 0):
+                print(pygame.mouse.get_pressed())
+                draw_box(mouse_x - brush_size, mouse_y - brush_size, brush_size, brush_size, background, brush_color)
+
+            if pressed[pygame.K_0]:
+                print("0")
+                brush_color = color_zero
+            if pressed[pygame.K_1]:
+                brush_color = color_one
+            if pressed[pygame.K_2]:
+                brush_color = color_two
+            if pressed[pygame.K_3]:
+                brush_color = color_three
+            if pressed[pygame.K_4]:
+                brush_color = color_four
+            if pressed[pygame.K_5]:
+                brush_color = color_five
+            if pressed[pygame.K_6]:
+                brush_color = color_six
+            # if pressed[pygame.K_7]:
+            #     brush_color = color_zero
+            # if pressed[pygame.K_8]:
+            #     brush_color = color_zero
+            if pressed[pygame.K_9]:
+                brush_size +=1
+                print(brush_size)
+            if pressed[pygame.K_8]:
+                brush_size -= 1
+                print(brush_size)
 
         mouse_x, mouse_y = pygame.mouse.get_pos()
-
-        background.fill(RGB_mouse_color(800, 600))
-
-        draw_box(mouse_x - 20, mouse_y - 20, 20, 20, background, test_color)
-
         screen.blit(background, (0, 0))
         pygame.display.flip()
 
